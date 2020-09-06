9a469730b07ed05489551cc4ec8fbb21e6ab615a
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-09-06 16:26:04 +0900

    販売手数料と販売利益の計算機能

---
+++ app/javascript/calculation.js
@@ -0,0 +1,33 @@
+
+//入力した値を計算する処理
+function calculation(){
+
+    //入力値の要素を取得
+    const tagInput = document.getElementById("item-price");
+    console.log(tagInput);
+
+    //入力した時のイベント設定
+    tagInput.addEventListener('keyup', () => {
+    
+    //取得した要素を変数に代入
+    const amount = tagInput.value
+    console.log(amount)
+
+    //取得した値から手数料を計算
+    const tax_sum = Math.ceil(amount * 0.1);
+
+    //取得した値から利益率を計算
+    const profit_sum = Math.floor(amount * 0.9);
+    
+    //taxを別の入力フォームに表示する
+    const tax = document.getElementById("add-tax-price");
+    tax.innerHTML = tax_sum;
+
+    //profitを別の入力フォームに表示する
+    const profit = document.getElementById("profit");
+    profit.innerHTML = profit_sum;
+    
+ });
+}
+window.addEventListener("load", calculation);
+
