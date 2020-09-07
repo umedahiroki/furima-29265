8191d3cf48713351166fa1197720555f1a9a97a8
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-09-07 16:38:47 +0900

    商品一覧表示機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 before_action :move_to_index, except: [:index]
 
 def index
-  @item = Item.all
+  @item = Item.all.order("created_at DESC")
 end
 
 def new
