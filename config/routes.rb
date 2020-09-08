61c543fb9fd01a881f33f7c52e3a07fa59857fd2
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-09-08 11:15:02 +0900

    商品詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "items#index"
   devise_for :users
-  resources :items, only: [:index,:new,:create]
+  resources :items, only: [:index,:new,:create,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
