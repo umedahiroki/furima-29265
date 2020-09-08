40f39bb566dbffc3c0bdccc8425fd54020f6957e
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-09-08 16:42:48 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 before_action :move_to_index, except: [:index, :show]
-before_action :set_item, only: [:edit,:show]
+before_action :set_item, only: [:edit,:update,:show]
 
 def index
   @item = Item.all.order("created_at DESC")
@@ -28,8 +28,7 @@ def create
   end
 
   def update    
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to items_path
     else
       render :edit
